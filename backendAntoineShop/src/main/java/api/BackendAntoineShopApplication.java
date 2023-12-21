ca4cb6ef063001c8d034b8708bd66b67c5d732e2
Jakub <117046128+KLesiu@users.noreply.github.com>
2023-12-21 22:23:33 +0100

    Start mongoDb config

---
+++ backendAntoineShop/src/main/java/api/BackendAntoineShopApplication.java
@@ -0,0 +1,17 @@
+package api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class BackendAntoineShopApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAntoineShopApplication.class, args);
+
+	}
+
+}
