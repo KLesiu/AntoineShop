81929ea7e35d68180baaa8d1f9bac19a6874b7e5
Jakub <117046128+KLesiu@users.noreply.github.com>
2023-12-26 13:23:49 +0100

    Add get and post user method

--- backendAntoineShop/routers/routes.py
+++ backendAntoineShop/routers/routes.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter,HTTPException,status
-from dependencies import db_dependency,ItemBase
-from models.models import Item
+from dependencies import db_dependency,ItemBase,UserBase
+from models.models import Item,User
 
 router = APIRouter()
 
+
+# Items
 @router.post("/items/", status_code=status.HTTP_201_CREATED)
 async def create_item(item: ItemBase, db: db_dependency):
     db_item = Item(**item.dict())
@@ -17,3 +19,30 @@ async def read_item(item_id:int,db:db_dependency):
     if item is None:
         raise HTTPException(status_code=404,detail="Item not found")
     return item
+
+
+@router.delete("/items/{item_id}",status_code=status.HTTP_202_ACCEPTED)
+async def delete_item(item_id:int,db:db_dependency):
+    item = db.query(Item).filter(Item.id==item_id).first()
+    if item is None:
+        raise HTTPException(status_code=404,detail="Item not found")
+    return item + " deleted"
+
+# Users
+@router.post("/users/",status_code=status.HTTP_201_CREATED)
+async def create_user(user:UserBase,db:db_dependency):
+    db_user = User(**user.dict())
+    db.add(db_user)
+    db.commit()
+
+
+@router.get("/users/{user_id}",status_code=status.HTTP_200_OK)
+async def read_user(user_id:int,db:db_dependency):
+    user = db.query(User).filter(User.id==user_id).first()
+    if user is None:
+        raise HTTPException(status_code=404,detail="User not found")
+    return user
+
+
+
+    
