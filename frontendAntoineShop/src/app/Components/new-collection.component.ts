f377d9bfd87f4dcb59034623dc1979b41e07a0c6
Jakub <117046128+KLesiu@users.noreply.github.com>
2024-01-06 18:32:27 +0100

    Add new-collection-component and basic layout inside it

---
+++ frontendAntoineShop/src/app/Components/new-collection.component.ts
@@ -0,0 +1,23 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'new-collection-component',
+    standalone:true,
+    imports:[],
+    template:`
+        <section class="flex flex-col items-center mt-10">
+            <h2 class="text-3xl font-bold">NEW COLLECTION</h2>
+            <p class="text-gray-500">Our latest collection, where everyone can find someone for themselves</p>
+            <div class="w-full h-96 flex justify-between gap-5 flex-wrap mt-5 ">
+                <div class="w-96 h-72 bg-red-400"></div>
+                <div class="w-96 h-72 bg-red-400"></div>
+                <div class="w-96 h-72 bg-red-400"></div>
+                <div class="w-96 h-72 bg-red-400"></div>
+                <div class="w-96 h-72 bg-red-400"></div>
+                <div class="w-96 h-72 bg-red-400"></div>
+            </div>
+        </section>
+    `
+
+})
+export class NewCollectionComponent{}
--- frontendAntoineShop/src/app/app.component.ts
+++ frontendAntoineShop/src/app/app.component.ts
@@ -3,15 +3,17 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from './Components/menu.component';
 import { SliderComponent } from './Components/slider.component';
+import { NewCollectionComponent } from './Components/new-collection.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,MenuComponent,SliderComponent],
+  imports: [CommonModule, RouterOutlet,MenuComponent,SliderComponent,NewCollectionComponent],
   template: `
   <div class="max-w-7xl flex justify-center mx-auto flex-wrap">
       <menu-component class="w-full"/>
       <slider-component class="w-full"/>
+      <new-collection-component class="w-full" />
 
   </div>
   `,
